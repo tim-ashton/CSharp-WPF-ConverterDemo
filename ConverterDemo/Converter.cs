2249107809f7f87a8165d9a6e602500c3fe54429
tim_ashton <t.j.ashton1@gmail.com>
2016-04-13 21:46:40 +1000

    Initial Commit

---
+++ ConverterDemo/Converter.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+namespace WpfApplication1
+{
+    class Converter : IValueConverter
+    {
+        /// <summary>
+        /// Converts the object to a string
+        /// </summary>
+        /// <param name="value">The object to convert</param>
+        /// <param name="targetType">unused</param>
+        /// <param name="parameter">unused</param>
+        /// <param name="culture">unused</param>
+        /// <returns></returns>
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value != null)
+            {
+                return value.ToString();
+            }
+            return Binding.DoNothing;
+        }
+
+        /// <summary>
+        /// Convert the object to a decimal value.
+        /// </summary>
+        /// <param name="value">The object to convert</param>
+        /// <param name="targetType">unused</param>
+        /// <param name="parameter">unused</param>
+        /// <param name="culture">unused</param>
+        /// <returns></returns>
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string data = value as string;
+            if (data == null)
+            {
+                return value;
+            }
+            if (data.Equals(string.Empty))
+            {
+                return 0;
+            }
+            if (!string.IsNullOrEmpty(data))
+            {
+                decimal result;
+                //Hold the value if ending with .
+                if (data.EndsWith(".") || data.Equals("-0"))
+                {
+                    return Binding.DoNothing;
+                }
+                if (decimal.TryParse(data, out result))
+                {
+                    return result;
+                }
+            }
+            return Binding.DoNothing;
+        }
+
+    }
+}
---
+++ ConverterDemo/MainWindowViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApplication1
+{
+    class MainWindowViewModel
+    {
+
+        private double? myDouble;
+        public double? MyDouble
+        {
+            get { return myDouble; }
+            set
+            {
+                myDouble = value;
+            }
+        }
+    }
+}
+
+class MainWindowViewModel
+{
+
+    private double? myDouble;
+    public double? MyDouble
+    {
+        get { return myDouble; }
+        set
+        {
+            myDouble = value;
+        }
+    }
+}
